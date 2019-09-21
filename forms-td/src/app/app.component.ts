34cedcb38b2afb52e2607c6e3f096b0300969bb2
Antonio Cecere <antocecere77@gmail.com>
2019-09-21 18:15:46 +0200

    Aggiunta sezione summary con i dati inseriti dopo il submit

--- forms-td/src/app/app.component.ts
+++ forms-td/src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -36,7 +44,13 @@ export class AppComponent {
   // }
 
    onSubmit(form: NgForm) {
+    this.submitted = true;
     console.log(this.signupForm);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
    }
 
 }
