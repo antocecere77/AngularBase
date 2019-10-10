38805a7bfd7807ff90be078d98fc5ae7d12f29fe
Antonio Cecere <antocecere77@gmail.com>
2019-10-10 06:59:51 +0200

    Gestione errori post con i subject

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -1,23 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post} from './post.model';
 import { PostsService } from './post.service';
 import { post } from 'selenium-webdriver/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetching = false;
   error = null;
+  private errorSub: Subscription;
 
   constructor(private http: HttpClient, private postService: PostsService) {}
 
   ngOnInit() {
+
+    this.errorSub = this.postService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    });
+
     this.postService.fetchPosts().subscribe(posts => {
       this.isFetching = false;
       this.loadedPosts = posts;
@@ -33,18 +40,24 @@ export class AppComponent implements OnInit {
   onFetchPosts() {
     this.isFetching = true;
     // Send Http request
-    this.postService.fetchPosts().subscribe(posts => {
+    this.postService.fetchPosts().subscribe(
+      posts => {
       this.isFetching = false;
       this.loadedPosts = posts;
-    }, error => {
-      this.error = error.message;
-      console.log(error);
-    });
+      },
+      error => {
+        this.error = error.message;
+        console.log(error);
+      });
   }
 
   onClearPosts() {
     this.postService.deletePosts().subscribe(() => {
       this.loadedPosts = [];
     });
+  }
+
+  ngOnDestroy() {
+    this.errorSub.unsubscribe();
   }
 }
