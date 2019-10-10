16a05ba3e20c5a0ae0be41eb4905524904c60790
Antonio Cecere <antocecere77@gmail.com>
2019-10-10 07:06:19 +0200

    Aggiunta gestione errori con catchError

--- http/src/app/post.service.ts
+++ http/src/app/post.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post.model';
-import { map } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -38,7 +38,11 @@ export class PostsService {
             }
           }
           return postsArray;
-        }));
+        },
+        catchError(errorRes => {
+          return throwError(errorRes);
+        })
+      ));
       }
 
     deletePosts() {
