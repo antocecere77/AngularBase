3055aa7211865cba53f0683a452e3a6ab62f10aa
Antonio Cecere <antocecere77@gmail.com>
2019-10-11 07:12:15 +0200

    Aggiunto header a tutte le request

--- http/src/app/auth-interceptor.service.ts
+++ http/src/app/auth-interceptor.service.ts
@@ -4,7 +4,11 @@ export class AuthInterceptorService implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         console.log('Request in on its way');
-        return next.handle(req);
+        const modifiedRequest = req.clone({
+            headers: req.headers.append('Auth', 'xyz')
+        });
+
+        return next.handle(modifiedRequest);
     }
 
 }
