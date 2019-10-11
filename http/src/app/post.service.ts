a5b5c6a5407144f3c9e4c8aca8c1a251b11a8430
Antonio Cecere <antocecere77@gmail.com>
2019-10-11 06:46:23 +0200

    Aggiunto observe per differenti tipi di risposte

--- http/src/app/post.service.ts
+++ http/src/app/post.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams, HttpEventType } from '@angular/common/http';
 import { Post } from './post.model';
-import { map, catchError } from 'rxjs/operators';
+import { map, catchError, tap } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
@@ -18,7 +18,9 @@ export class PostsService {
         this.http
           .post<{name: string}>(
             'https://recipe-list-96492.firebaseio.com/posts.json',
-            postData
+            postData, {
+              observe: 'response'
+            }
           )
           .subscribe(responseData => {
             console.log(responseData);
@@ -56,6 +58,17 @@ export class PostsService {
     }
 
     deletePosts() {
-        return this.http.delete('https://recipe-list-96492.firebaseio.com/posts.json');
+        return this.http.delete('https://recipe-list-96492.firebaseio.com/posts.json',
+        {
+          observe: 'events'
+        }).pipe(tap(event => {
+          console.log(event);
+          if (event.type === HttpEventType.Sent) {
+            console.log(event.type);
+          }
+          if (event.type === HttpEventType.Response) {
+            console.log(event.body);
+          }
+        }));
     }
 }
