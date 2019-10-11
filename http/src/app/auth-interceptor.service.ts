368f17e05f46d150091a77be1a2d0f0b82831bf2
Antonio Cecere <antocecere77@gmail.com>
2019-10-11 07:27:23 +0200

    Aggiunto logging interceptor

--- http/src/app/auth-interceptor.service.ts
+++ http/src/app/auth-interceptor.service.ts
@@ -4,18 +4,11 @@ import { tap } from 'rxjs/operators';
 export class AuthInterceptorService implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-        console.log('Request in on its way');
         const modifiedRequest = req.clone({
             headers: req.headers.append('Auth', 'xyz')
         });
 
-        return next.handle(modifiedRequest).pipe(tap(event => {
-            console.log(event);
-            if (event.type === HttpEventType.Response) {
-                console.log('Response arrived, body data: ');
-                console.log(event.body);
-            }
-        }));
+        return next.handle(modifiedRequest);
     }
 
 }
